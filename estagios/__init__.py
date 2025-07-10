35c74be60bd6063827e0b3af46e30ad6004df702
Bruna Xavier <bxs@discente.ifpe.edu.br>
2025-07-10 19:57:26 -0300

    refactor: ajuste na sessão de login

--- estagios/__init__.py
+++ estagios/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
@@ -25,6 +25,9 @@ migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+login_manager.login_view = 'auth_bp.login'
+
+from .models import User, RoleEnum # <-- Mova esta importação para aqui ou importe db/bcrypt/mail onde precisar nos models
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
